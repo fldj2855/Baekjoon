65ff43a1b108cdfc66357d42b17171c33fa3f56e
fldj2855 <fldj2855@gmail.com>
2022-02-06 17:05:23 -0800

    9465

---
+++ 9465/9465.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<algorithm>
+#define endl '\n'
+#define MAX(A, B) (A > B ? A : B)
+
+using namespace std;
+
+int dp[2][100005];
+int N[2][100005];
+
+int main(void)
+{
+	ios_base :: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int T, n;
+	cin >> T;
+	for(int i = 0; i < T; ++i)
+	{
+		cin >> n;
+		for(int j = 0; j < 2; ++j)
+		{
+			for(int z = 0; z < n; ++z)
+			{
+				cin >> N[j][z];
+			}
+		}
+
+		dp[0][0] = N[0][0];
+		dp[1][0] = N[1][0];
+		if(n == 1)
+		{
+			cout << MAX(N[0][0], N[1][0]) << endl;
+            continue;
+		}
+		else
+		{
+			dp[0][1] = N[1][0] + N[0][1];
+			dp[1][1] = N[0][0] + N[1][1];
+
+			for(int j = 2; j < n; ++j)
+			{
+				dp[0][j] = MAX(dp[1][j-1], dp[1][j-2])+N[0][j];
+				dp[1][j] = MAX(dp[0][j-1], dp[0][j-2])+N[1][j];
+			}
+		}
+
+		// for(int i = 0; i < 2; ++i)
+		// {
+		// 	for(int j = 0; j < n; ++j)
+		// 		cout << dp[i][j] << ' ';
+		// 	cout << endl;
+		// }
+		cout << MAX(dp[0][n-1],dp[1][n-1]) << endl;
+	}
+
+
+	return 0;
+}
